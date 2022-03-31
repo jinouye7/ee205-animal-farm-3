aa2bc33ec4fe0f9aad9fcb008d168afc1fd581ff
jinouye7 <jinouye7@hawaii.edu>
2022-03-31 10:59:00 -1000

    declared new addCats function in addCats for use with linked list

--- addCats.h
+++ addCats.h
@@ -1,15 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///         University of Hawaii, College of Engineering
-/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+/// @brief  ee205_lab_10d_animal_farm_2 - EE 205 - Spr 2022
 ///
 /// @file addCats.h
 /// @version 1.0
 ///
 /// @author Jared Inouye <jinouye7@hawaii.edu>
-/// @date   09_Mar_2022
+/// @date   31_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
 #include <stdbool.h>
 #include "catDatabase.h"
 
+extern bool addCats(Cat* newCat);
 int addCat(const char name[], const enum Gender gender, const enum Breed breed, const bool isFixed, const Weight weight, const enum Color collarColor1, const enum Color collarColor2, const unsigned long long license, const char *date);
 
