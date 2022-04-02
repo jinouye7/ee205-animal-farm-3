4a20a39ecf229408238e859d866c77acc019ebfb
jinouye7 <jinouye7@hawaii.edu>
2022-04-01 17:44:13 -1000

    declared deleteCats and deleteAllCats functions

--- deleteCats.h
+++ deleteCats.h
@@ -1,14 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///         University of Hawaii, College of Engineering
-/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+/// @brief  ee205_lab_10d_animal_farm_2 - EE 205 - Spr 2022
 ///
 /// @file deleteCats.h
 /// @version 1.0
 ///
 /// @author Jared Inouye <jinouye7@hawaii.edu>
-/// @date   09_Mar_2022
+/// @date   01_APR_2022
 ///////////////////////////////////////////////////////////////////////////////
 
+#include "Cats.h"
 
-int deleteAllCats();
+bool deleteAllCats();
+bool deleteCat( Cats* name);
 
