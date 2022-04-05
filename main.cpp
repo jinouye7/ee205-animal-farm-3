92608fbd1af5a3ce4ac393ed2c2d0c46371ca49b
jinouye7 <jinouye7@hawaii.edu>
2022-04-04 16:47:48 -1000

    removed old main code

--- main.cpp
+++ main.cpp
@@ -139,64 +139,7 @@ int main() {
 
     //addCats(new Cats("test", UNKNOWN_GENDER, PERSIAN, 1.0));
 #endif
-
-
-#ifdef OLD
-    addCat( "Loki", MALE, PERSIAN, true, 8.5,  BLACK, WHITE, 101, "May 4, 2000") ;
-    addCat( "Milo", MALE, MANX, true, 7.0 ,  BLACK, RED, 102, "May 4, 2000") ;
-    addCat( "Bella", FEMALE, MAINE_COON, true, 18.2 , BLACK, BLUE, 103, "May 4, 2000") ;
-    addCat( "Kali", FEMALE, SHORTHAIR, false, 9.2 , BLACK, GREEN, 104, "May 4, 2000") ;
-    addCat( "Trin", FEMALE, MANX, true, 12.2, BLACK, PINK, 105, "May 4, 2000") ;
-    addCat( "Chili", UNKNOWN_GENDER, SHORTHAIR, false, 19.0, WHITE, BLACK, 106, "May 4, 2000") ;
-
-
-    printAllCats();
-
-    int kali = findCat( "Kali" ) ;
-    assert(updateCatName( kali, "Chili" ) == 1) ; // this should fail
-    assert(printCat( kali ) == 0);
-    assert(updateCatName( kali, "Capulet" ) == 0) ;
-    assert(updateCatWeight( kali, 9.9 ) == 0) ;
-    assert(fixCat( kali ) == 0) ;
-    assert(updateCatCollar1( kali, RED) == 0);
-    assert(updateCatCollar2( kali, BLUE) == 0);
-    assert(updateLicense( kali, 111) == 0);
-    assert(updateBirthday( kali, "March 17,2000") == 0);
-    assert(printCat( kali ) == 0);
-    assert(printAllCats() == 0);
-    assert(deleteAllCats() == 0);
-    assert(printAllCats() == 1); //this should fail (all cats deleted)
-
-#endif
-
-
-//testing birthday stuff
-
-#ifdef BIRTHDAY
-
-    //testing string to struct tm and struct tm to string functions
-       //birthdayToStruct(1, "May 4, 2010");
-       //structToDate(1);
-       //testing addCat
-       addCat( "Loki", MALE, PERSIAN, true, 8.5,  BLACK, WHITE, 101, "May 4, 2000") ;
-       addCat( "Milo", MALE, MANX, true, 7.0 ,  BLACK, RED, 102, "December 10, 2001") ;
-       addCat( "Bella", FEMALE, MAINE_COON, true, 18.2 , BLACK, BLUE, 103, "March 17, 2000") ;
-
-       //test report cat
-       printAllCats();
-       printCat(1);
-       //testing update cat
-       updateBirthday(1, "January 15, 2003" );
-       //test report cat again
-       printCat(1);
-
-#endif
-
-
-
     return 0;
-//yay
-
 }
 
 
