8c4c39bbf429f1280d1eb01af69bcff685bd6c5c
jinouye7 <jinouye7@hawaii.edu>
2022-04-04 16:43:00 -1000

    removed unused functions from catDatabase.cpp

--- catDatabase.cpp
+++ catDatabase.cpp
@@ -59,7 +59,7 @@ extern bool validateDatabase() {
 }
 
 ////////////////////////////////////// old code /////////////////////////////////////////////////////
-struct Cat cats[MAXCATS];
+
 
 
 //returns string counterpart for gender enum
@@ -150,63 +150,3 @@ char* colorName (const enum Color color) {
 }
 
 
-int birthdayToStruct(const int index, const char *date){
-
-    char* months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-
-
-    memset(&cats[index].birthday, 0, sizeof cats[index].birthday);  // set all fields to 0
-    char str[4];
-    sscanf(date,"%3s",str);
-    int i=0;
-    for(i=0; i<12; i++)
-    {
-        if(!strcmp(str,months[i]))
-        {
-            //printf("Your month number is: %d",i+1);
-            int value;
-            value = i +1 ;
-            cats[index].birthday.tm_mon = value;
-            //debug
-            //printf("storedmonth: %d\n", cats[index].birthday.tm_mon);
-
-        }
-
-
-    }
-
-    //* means it will be read but not stored
-    sscanf(date, "%*s %d, %d", &cats[index].birthday.tm_mday, &cats[index].birthday.tm_year);
-
-    //debug
-    //printf("storedday: %d\n", cats[index].birthday.tm_mday);
-    //printf("storedyear: %d\n", cats[index].birthday.tm_year);
-
-
-    // Adjust to struct tm references
-    cats[index].birthday.tm_year -= 1899;
-    cats[index].birthday.tm_mon--;
-
-    return 0;
-}
-
-
-int structToDate(const int index){
-    //char buf[200];
-    //strftime(buf, 200, "struct birthday: %a %b %d %r %Z %G\n", &cats[index].birthday);
-    //puts(buf);
-
-    int year;
-    int month;
-    int day;
-
-    year = cats[index].birthday.tm_year + 1899;
-    month = cats[index].birthday.tm_mon + 1;
-    day = cats[index].birthday.tm_mday;
-
-    printf("birthday = [%d/%d/%d]\n", day, month, year);
-
-
-
-    return 0;
-}
