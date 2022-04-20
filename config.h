ac2e1e942a967dccc5e2a6c0106d0950aeca9729
jinouye7 <jinouye7@hawaii.edu>
2022-04-19 16:59:44 -1000

    added dump formatting macros to config.h

--- config.h
+++ config.h
@@ -10,10 +10,29 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #pragma once
-#define PROGRAM_NAME "animalFarm2"
+#include <iomanip>
+#define PROGRAM_TITLE "Animal Farm 3"
+#define PROGRAM_NAME "animalFarm3"
+#define FORMAT_LINE_FOR_DUMP(className, member) std::cout << std::setfill( ' ' )  /* Space pad    */  \
+                                                          << std::left            /* Left justify */  \
+                                                          << std::boolalpha  /* Print `true` or `false` for `bool`s */ \
+                                                          << std::setw( 8) << (className)             \
+                                                          << std::setw(20) << (member)                \
+                                                          << std::setw(52)  /* (data) */
+
+#define PRINT_HEADING_FOR_DUMP std::cout << std::setw(80) << std::setfill( '=' ) << "" << std::endl
+
+
+
+
+
+
 #define MAXLENGTH 50 /// maximum name length
 #define MAXCATS 1024 /// maximum number of cats
 
+
+
+
 typedef float Weight ;
 typedef int NumCats;
-const Weight UNKNOWN_WEIGHT = -1 ;  /// Weight is unknown
+//const Weight UNKNOWN_WEIGHT = -1 ;  /// Weight is unknown
